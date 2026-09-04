ea0d014fc8f2eb39e1bdee4b3e3edff7f59fe229
Altair Li <137127498+Altairpaca@users.noreply.github.com>
2026-09-04 17:40:20 +0800

    feat: export SkillBench normalization bridge

--- src/agent_harness_index/__init__.py
+++ src/agent_harness_index/__init__.py
@@ -2,18 +2,28 @@ from .aggregate import summarize
 from .compare import COMPARISON_SCHEMA_VERSION, compare_cells
 from .dataset import DATASET_SCHEMA_VERSION, dataset_fingerprint, inspect_dataset, observation_identity
 from .model import Observation, SCHEMA_VERSION, configuration_fingerprint, environment_fingerprint, mapping_fingerprint
+from .skillbench import (
+    SKILLBENCH_IMPORT_SCHEMA_VERSION,
+    SKILLBENCH_OBSERVATION_SCHEMA_VERSION,
+    SkillBenchDiagnostic,
+    normalize_skillbench_observation,
+)
 
 __all__ = [
     "COMPARISON_SCHEMA_VERSION",
     "DATASET_SCHEMA_VERSION",
     "Observation",
     "SCHEMA_VERSION",
+    "SKILLBENCH_IMPORT_SCHEMA_VERSION",
+    "SKILLBENCH_OBSERVATION_SCHEMA_VERSION",
+    "SkillBenchDiagnostic",
     "compare_cells",
     "configuration_fingerprint",
     "dataset_fingerprint",
     "environment_fingerprint",
     "inspect_dataset",
     "mapping_fingerprint",
+    "normalize_skillbench_observation",
     "observation_identity",
     "summarize",
 ]
